6f16784faa192ab7482148b55e266f69ec4c0d71
Mehdi Charouh <mcharouh@e2r10p2.1337.ma>
2023-03-15 19:09:12 +0100

    shitty code

--- CPP00/ex00/Megaphone.cpp
+++ CPP00/ex00/Megaphone.cpp
@@ -4,13 +4,12 @@ int main(int ac, char **str)
 {
 	if (ac == 2)
 	{
-		int i = 0;
 		while (*str[1])
 		{
 			while (std::islower(*str[1]))
 				*str[1] -= 32;
 			std::cout << *str[1];
-			*str[1]++;
+			char c = *str[1]++;
 		}
 		std::cout << std::endl;
 		return 0;
