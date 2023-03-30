90e83e75d19e20a6185db22efab08fdb5adb7744
Mehdi Charouh <mcharouh@e2r6p8.1337.ma>
2023-03-30 02:47:37 +0000

    finished 03

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcharouh <mcharouh@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 02:25:41 by mcharouh          #+#    #+#             */
+/*   Updated: 2023/03/30 02:47:16 by mcharouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+int main ()
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string *stringPTR = &brain;
+	std::string& stringREF = brain;
+	
+	std::cout << "This is the stringREF address : " << &stringREF << std::endl;
+	std::cout << "This is the stringPTR address : " << stringPTR << std::endl;
+	std::cout << "This is the string address : " <<  &brain << std::endl;
+
+	std::cout << "This is the stringREF value : " << stringREF << std::endl;
+	std::cout << "This is the stringPTR value : " << *stringPTR << std::endl;
+	std::cout << "This is the string value : " <<brain << std::endl;
+	
+	return 0;
+}
