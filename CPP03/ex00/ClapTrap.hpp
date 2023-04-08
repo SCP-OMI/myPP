bf6e3bc2f92fea74cf9a6de74c017dfd7b399d4e
Mehdi Charouh <mcharouh@e2r6p8.1337.ma>
2023-04-08 06:31:45 +0000

    started working on CPP03

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcharouh <mcharouh@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 06:28:40 by mcharouh          #+#    #+#             */
+/*   Updated: 2023/04/08 06:31:05 by mcharouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap{
+	private :
+		std::string name;
+		int Hit_points;
+		int Energy_points;
+		int Attack_damage;
+	public :
+}
+
+
+
+
+#endif
