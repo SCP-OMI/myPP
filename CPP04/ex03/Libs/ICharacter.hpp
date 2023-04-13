eb0aa0ab2f5e79841fb3cef00238e59e297d76c1
SCP-Omi <mehdicharouh@gmail.com>
2023-04-13 06:04:32 +0000

    tweaks

--- CPP04/ex03/Libs/ICharacter.hpp
+++ CPP04/ex03/Libs/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: mcharouh <mcharouh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 08:10:59 by mcharouh          #+#    #+#             */
-/*   Updated: 2023/04/12 08:54:31 by mcharouh         ###   ########.fr       */
+/*   Updated: 2023/04/13 06:03:31 by mcharouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 
 #include "../Libs/AMateria.hpp"
 
-class	ICharacter{
+class AMateria;
+
+class	ICharacter
+{
 	public:
 		virtual ~ICharacter() {}
 		virtual std::string const & getName() const = 0;
-		virtual void equip(AMateria* Skill) = 0;
-		virtual void unequip(int i) = 0;
-		virtual void use(int i, ICharacter& target) = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
 };
 
-
 #endif
+
